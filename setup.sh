9cfa2d8a5d07ed49311fa64ebb1853cb2ef11c81
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-12-26 22:55:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,8 +24,8 @@ sed -i -e "s@<sShopDir>@${OXID_PATH}/source@g; s@<sCompileDir>@${OXID_PATH}/sour
 sed -i -e "s@partial_module_paths:@partial_module_paths: ${TARGET_PATH}@g" test_config.yml
 sed -i -e "s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
 #cat test_config.yml
-#CLEAN_DB_HOST=${/$DB_HOST/;*/}
-#echo clean host: $CLEAN_DB_HOST
+CLEAN_DB_HOST=${DB_HOST/;*/}
+echo clean host: $CLEAN_DB_HOST
 composer config repositories.build path "${BUILD_DIR}"
 
 #just in case the module has private repository dependencies clone that config
@@ -48,8 +48,8 @@ echo "installing ${PACKAGE_NAME} in ${TARGET_PATH}"
 composer require "${PACKAGE_NAME}:$VERSION"
 
 echo loading DB
-mysql -u $DB_USER -p$DB_PWD -h $DB_HOST $DB_NAME < vendor/oxid-esales/oxideshop-ce/source/Setup/Sql/database_schema.sql
-mysql -u $DB_USER -p$DB_PWD -h $DB_HOST $DB_NAME < vendor/oxid-esales/oxideshop-ce/source/Setup/Sql/initial_data.sql
+mysql -u $DB_USER -p$DB_PWD -h $CLEAN_DB_HOST $DB_NAME < vendor/oxid-esales/oxideshop-ce/source/Setup/Sql/database_schema.sql
+mysql -u $DB_USER -p$DB_PWD -h $CLEAN_DB_HOST $DB_NAME < vendor/oxid-esales/oxideshop-ce/source/Setup/Sql/initial_data.sql
 vendor/bin/oe-eshop-db_migrate migrations:migrate
 vendor/bin/oe-eshop-db_views_generate
 
