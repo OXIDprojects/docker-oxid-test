bd21bac2347b5541006d3c8ab729378e84b352b7
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-12-28 01:46:52 +0100

    Create staticBoot.php

---
+++ staticBoot.php
@@ -0,0 +1,4 @@
+<?php
+require_once __PATH__ . 'vendor/oxid-esales/testing-library/base.php';
+require_once __PATH__ . 'vendor/oxid-esales/oxideshop-ce/source/oxfunctions.php';
+require_once __PATH__ . 'autoload.oxid.php';
