f36f844a775942d91dc646c7dfccf8031c3f4ae1
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-12-30 17:03:40 +0100

    Create activateModule.sh

---
+++ activateModule.sh
@@ -0,0 +1,14 @@
+#! bin/bash
+set -e
+set -x
+if [ -z "$MODULE_NAME" ]
+then
+echo showing id from metadata.php:
+cat $BUILD_DIR/metadata.php |grep "\s*'id'\s*="
+echo module name to activate?
+read MODULE_NAME
+fi
+#oxrun does not work on oxid 6.2 
+#php oxrun.phar module:activate $MODULE_NAME
+vendor/bin/oxid module:activate $MODULE_NAME
+vendor/bin/oxid-dump-autoload
