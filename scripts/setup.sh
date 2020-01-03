2bfb0fe0ead42c6942a6aa353ee98aa843e47da0
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2020-01-03 13:45:58 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 
-bash /var/www/oxid/scripts/setupOxid.sh
-bash /var/www/oxid/scripts/setupPackage.sh
-bash /var/www/oxid/scripts/activateModule.sh
+bash /var/www/oxidesales/scripts/setupOxid.sh
+bash /var/www/oxidesales/scripts/setupPackage.sh
+bash /var/www/oxidesales/scripts/activateModule.sh
