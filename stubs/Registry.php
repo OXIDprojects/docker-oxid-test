262583cd3feb5d45ec0543f728a3d2524141ff4f
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2020-01-07 23:50:22 +0100

    improve stubs
    add set method to avoid phpstan false positives

--- stubs/Registry.php
+++ stubs/Registry.php
@@ -5,24 +5,33 @@ class Registry {
    /**
      * @template T
      * @param class-string<T> $className The class name from the Unified Namespace.
-     * param mixed  ...$args   constructor arguments
      *
      * @static
      *
-     * @return object
-     * @return T
+     * @return T|null
      */
     public static function get($className)
     {
     }
 
-    /** @template T
-     * @param class-string<T> $className A unified namespace class name
-     * param mixed  ...$args   constructor arguments
+   /**
+     * @template T
+     * @param class-string<T> $className The class name from the Unified Namespace.
+     * @param T|null $value
      *
      * @static
      *
-     * @return mixed
+     * @return void
+     */
+    public static function set($className, $value)
+    {
+    }
+
+    /** @template T
+     * @param class-string<T> $className A unified namespace class name
+     *
+     * @static
+     *
      * @return T
      */
     protected static function getObject($className)
