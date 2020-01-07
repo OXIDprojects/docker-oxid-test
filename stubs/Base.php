da9d2619ffe74cc4eda59641d42a4c330d012cab
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2020-01-08 00:21:31 +0100

    improve Base stub
    allow setting config to null

--- stubs/Base.php
+++ stubs/Base.php
@@ -3,11 +3,18 @@ namespace OxidEsales\EshopCommunity\Core;
 
 class Base
 {
-     /**
-     *
+    /**
      * @return \OxidEsales\Eshop\Application\Model\User|false
      */
     public function getUser()
+    {
+    }
+
+    /**
+     * @param \OxidEsales\Eshop\Core\Config|null $config
+     * @return void
+     */
+    public function setConfig($config)
     {
     }
 }
