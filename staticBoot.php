d8b7d71309d4c71f720758d91910a24ee97b6127
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2020-01-08 19:59:19 +0100

    add overridablefunctions

--- staticBoot.php
+++ staticBoot.php
@@ -1,4 +1,5 @@
 <?php
 require_once 'vendor/oxid-esales/testing-library/base.php';
 require_once 'vendor/oxid-esales/oxideshop-ce/source/oxfunctions.php';
+require_once 'vendor/oxid-esales/oxideshop-ce/source/overridablefunctions.php';
 require_once 'autoload.oxid.php';
